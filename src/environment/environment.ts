44c3d0060c7fc66136a330105b51d47bed8de13f
takyifidelis <takyifidelis@gmail.com>
2026-09-07 18:54:32 +0000

    fix: revert API endpoint to production URL

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // api: 'https://api.tripsecureagrifinanceltd.com/api',
-  api: 'https://agrifinance-backend-staging.new-app.workers.dev/api',
+  api: 'https://api.tripsecureagrifinanceltd.com/api',
 };
